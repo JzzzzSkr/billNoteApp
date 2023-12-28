54d01d37930bc7c6ca937bfd1d0308f34bcceee2
JzzzzSkr <harisskr@outlook.com>
2023-12-28 14:39:00 +1100

    will start at month

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,18 @@ import Month from "@/pages/Month";
 import New from "@/pages/New";
 import Year from "@/pages/Year";
 import { createBrowserRouter } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
     children: [
+      {
+        index: true,
+        element: <Navigate to="/month" />,
+      },
       {
         path: "month",
         element: <Month></Month>,
